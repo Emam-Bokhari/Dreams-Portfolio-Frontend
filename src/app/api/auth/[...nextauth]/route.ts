7a7c549701f5343b0055af0a3cb3a18ece791b3f
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-14 11:53:21 +0600

    fix: fix next auth type mis match problem

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,12 @@
-import { authOptions } from "@/utils/authOptions"
-import NextAuth from "next-auth"
+// module code
+// import { authOptions } from "@/utils/authOptions"
+// import NextAuth from "next-auth"
 
-export const handler = NextAuth(authOptions)
+// export const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+// export { handler as GET, handler as POST }
 
+// Ph support team code
 // import { authOptions } from "@/utils/authOptions";
 // import NextAuth, { NextAuthOptions } from "next-auth";
 // import { NextRequest, NextResponse } from "next/server";
@@ -13,4 +15,36 @@ export { handler as GET, handler as POST }
 
 // export const GET = (req: NextRequest) => handler(req);
 // export const POST = (req: NextRequest) => handler(req);
+
+
+
+import NextAuth, { NextAuthOptions } from "next-auth"
+
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ],
+    pages: {
+        signIn: "/login",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
+
+
+
+
 
